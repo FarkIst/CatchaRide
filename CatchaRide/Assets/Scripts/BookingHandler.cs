1a4fc28c0dd62a326b81e924fa9c1055f78d18d7
FarkIst <FarkIst@github.com>
2019-11-12 23:05:31 +0100

    add main menu, initial driver scene

--- CatchaRide/Assets/Scripts/BookingHandler.cs
+++ CatchaRide/Assets/Scripts/BookingHandler.cs
@@ -28,9 +28,6 @@ public class BookingHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        PrintClient();
-        PrintDriver();
-
         if (!_locationHandler)
             _locationHandler = FindObjectOfType<LocationHandler>();
     }
@@ -42,128 +39,6 @@ public class BookingHandler : MonoBehaviour
         _longitude = _locationHandler.Longitude;
     }
 
-
-    //Debug
-    public void PrintClient()
-    {
-        
-        List<Client> clients = new List<Client>();
-
-        Client c1 = new Client
-        {
-            Name = "Vladimir",
-            Status = "Accepted",
-            Coordinates = new Coordinates
-            {
-                Latitude = 55.1023,
-                Longitude = 12.2111
-            }
-        };
-
-        Client c2 = new Client
-        {
-            Name = "Stefano",
-            Status = "Declined",
-            Coordinates = new Coordinates
-            {
-                Latitude = 55.1023,
-                Longitude = 12.2111
-            }
-        };
-
-        Client c3 = new Client
-        {
-            Name = "Arcadio",
-            Status = "Declined",
-            Coordinates = new Coordinates
-            {
-                Latitude = 55.1023,
-                Longitude = 12.2111
-            }
-        };
-        Client c4 = new Client
-        {
-            Name = "Seeeeeean",
-            Status = "Accepted",
-            Coordinates = new Coordinates
-            {
-                Latitude = 55.1023,
-                Longitude = 12.2111
-            }
-        };
-
-        clients.Add(c1);
-        clients.Add(c2);
-        clients.Add(c3);
-        clients.Add(c4);
-
-        ClientContainer clientContainer = new ClientContainer() { Clients = clients};
-
-        string savePath = "./Assets/clients";
-
-        using (StreamWriter stream = new StreamWriter(savePath + ".json"))
-        {
-            string json = JsonUtility.ToJson(clientContainer);
-            stream.Write(json);
-        }
-    }
-
-    public void PrintDriver()
-    {
-
-        List<Driver> drivers = new List<Driver>();
-
-        Driver d1 = new Driver
-        {
-            Name = "Saaeeen",
-            Distance = "2km",
-            Status = "Accepted",
-            Coordinates = new Coordinates
-            {
-                Latitude = 55.12211,
-                Longitude = 13.112
-            }
-        };
-
-        Driver d2 = new Driver
-        {
-            Name = "Salami",
-            Distance = "20km",
-            Status = "Declined",
-            Coordinates = new Coordinates
-            {
-                Latitude = 55.12211,
-                Longitude = 13.112
-            }
-        };
-
-        Driver d3 = new Driver
-        {
-            Name = "Kebab",
-            Distance = "1km",
-            Status = "Processing",
-            Coordinates = new Coordinates
-            {
-                Latitude = 55.12211,
-                Longitude = 13.112
-            }
-        };
-
-
-        drivers.Add(d1);
-        drivers.Add(d2);
-        drivers.Add(d3);
-
-        DriverContainer driverContainer = new DriverContainer() { Drivers = drivers};
-
-        string savePath = "./Assets/drivers";
-
-        using (StreamWriter stream = new StreamWriter(savePath + ".json"))
-        {
-            string json = JsonUtility.ToJson(driverContainer);
-            stream.Write(json);
-        }
-    }
 
     //Post method
     public void CreateBooking()
@@ -196,4 +71,142 @@ public class BookingHandler : MonoBehaviour
         .Catch(err => this.LogMessage("Error", err.Message));
     }
 
+
+    //Get Method
+    public List<Client> GetAllClients()
+    {
+        List<Client> _clients = new List<Client>();
+        RestClient.Get<ClientContainer>(basePath).Then(res =>
+        {
+            foreach(Client c in res.Clients)
+            {
+                _clients.Add(c);
+            }
+        });
+        return _clients;
+    }
+
 }
+
+
+////Debug
+//public void PrintClient()
+//{
+
+//    List<Client> clients = new List<Client>();
+
+//    Client c1 = new Client
+//    {
+//        Name = "Vladimir",
+//        Status = "Accepted",
+//        Coordinates = new Coordinates
+//        {
+//            Latitude = 55.1023,
+//            Longitude = 12.2111
+//        }
+//    };
+
+//    Client c2 = new Client
+//    {
+//        Name = "Stefano",
+//        Status = "Declined",
+//        Coordinates = new Coordinates
+//        {
+//            Latitude = 55.1023,
+//            Longitude = 12.2111
+//        }
+//    };
+
+//    Client c3 = new Client
+//    {
+//        Name = "Arcadio",
+//        Status = "Declined",
+//        Coordinates = new Coordinates
+//        {
+//            Latitude = 55.1023,
+//            Longitude = 12.2111
+//        }
+//    };
+//    Client c4 = new Client
+//    {
+//        Name = "Seeeeeean",
+//        Status = "Accepted",
+//        Coordinates = new Coordinates
+//        {
+//            Latitude = 55.1023,
+//            Longitude = 12.2111
+//        }
+//    };
+
+//    clients.Add(c1);
+//    clients.Add(c2);
+//    clients.Add(c3);
+//    clients.Add(c4);
+
+//    ClientContainer clientContainer = new ClientContainer() { Clients = clients };
+
+//    string savePath = "./Assets/clients";
+
+//    using (StreamWriter stream = new StreamWriter(savePath + ".json"))
+//    {
+//        string json = JsonUtility.ToJson(clientContainer);
+//        stream.Write(json);
+//    }
+//}
+
+//public void PrintDriver()
+//{
+
+//    List<Driver> drivers = new List<Driver>();
+
+//    Driver d1 = new Driver
+//    {
+//        Name = "Saaeeen",
+//        Distance = "2km",
+//        Status = "Accepted",
+//        Coordinates = new Coordinates
+//        {
+//            Latitude = 55.12211,
+//            Longitude = 13.112
+//        }
+//    };
+
+//    Driver d2 = new Driver
+//    {
+//        Name = "Salami",
+//        Distance = "20km",
+//        Status = "Declined",
+//        Coordinates = new Coordinates
+//        {
+//            Latitude = 55.12211,
+//            Longitude = 13.112
+//        }
+//    };
+
+//    Driver d3 = new Driver
+//    {
+//        Name = "Kebab",
+//        Distance = "1km",
+//        Status = "Processing",
+//        Coordinates = new Coordinates
+//        {
+//            Latitude = 55.12211,
+//            Longitude = 13.112
+//        }
+//    };
+
+
+//    drivers.Add(d1);
+//    drivers.Add(d2);
+//    drivers.Add(d3);
+
+//    DriverContainer driverContainer = new DriverContainer() { Drivers = drivers };
+
+//    string savePath = "./Assets/drivers";
+
+//    using (StreamWriter stream = new StreamWriter(savePath + ".json"))
+//    {
+//        string json = JsonUtility.ToJson(driverContainer);
+//        stream.Write(json);
+//    }
+//}
---
+++ CatchaRide/Assets/Scripts/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserController : MonoBehaviour
+{
+    public string userName;
+    public string userType;
+
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("usercontroller");
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
