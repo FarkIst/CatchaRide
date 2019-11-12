9bf8f7f34840566a9edfa0b2cdda3bb15221ff80
FarkIst <cph-if36@cphbusiness.com>
2019-11-12 13:26:18 +0100

    init

---
+++ Assets/Scripts/LocationHandler.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LocationHandler : MonoBehaviour
+{
+    private PermissionHandler _permissionHandler;
+    double longitude, latitude;
+
+
+    private void Start()
+    {
+        _permissionHandler = GetComponent<PermissionHandler>();
+        _permissionHandler.PromptLocationPermissionRequest();
+
+        //Debug
+        longitude = 12.462724;
+        latitude = 55.638539;
+    }
+
+    IEnumerator GetLocation()
+    {
+        // First, check if user has location service enabled
+        if (!Input.location.isEnabledByUser)
+        {
+            _permissionHandler.PromptLocationPermissionRequest();
+            yield break;
+        }     
+
+
+        // Start service before querying location
+        Input.location.Start();
+
+        // Wait until service initializes
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        // Service didn't initialize in 20 seconds
+        if (maxWait < 1)
+        {
+            print("Timed out");
+            yield break;
+        }
+
+        // Connection has failed
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            print("Unable to determine device location");
+            yield break;
+        }
+        else
+        {
+            // Access granted and location value could be retrieved
+            latitude = Input.location.lastData.latitude;
+            longitude = Input.location.lastData.longitude;
+
+        }
+
+        // Stop service if there is no need to query location updates continuously
+        Input.location.Stop();
+    }
+
+    public void SendPosition()
+    {
+        Application.OpenURL("https://www.google.com/maps/dir/?api=1&" + latitude + ","+ longitude);
+
+    }
+}
---
+++ Assets/Scripts/PermissionHandler.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if PLATFORM_ANDROID
+using UnityEngine.Android;
+#endif
+
+public class PermissionHandler : MonoBehaviour
+{
+    GameObject dialog = null;
+
+    private bool isLocation = false;
+    private bool permissionsGranted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+#if UNITY_EDITOR
+        isLocation = true;
+#endif
+    }
+
+
+    private void Update()
+    {
+        if (Input.location.isEnabledByUser && !isLocation)
+            isLocation = true;
+    }
+
+
+    public void PromptLocationPermissionRequest()
+    {
+#if PLATFORM_ANDROID
+        if (!Input.location.isEnabledByUser)
+        {
+            Permission.RequestUserPermission(Permission.FineLocation);
+        }
+        else
+        {
+            isLocation = true;
+        }
+#endif
+    }
+
+    
+    void OnGUI()
+    {
+#if PLATFORM_ANDROID
+        if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
+        {
+            dialog.AddComponent<PermissionsRationaleDialog>();
+            return;
+        }
+        else if (dialog != null)
+        {
+            Destroy(dialog);
+        }
+#endif
+    }
+}
