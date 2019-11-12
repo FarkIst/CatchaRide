a61ab4371271c3cdd20c63e12b1bd5af34ae8207
VladMB <cph-mb402@cphbusiness.dk>
2019-11-12 21:54:50 +0100

    Added a few more route implementations

--- Engine/routes/booking.js
+++ Engine/routes/booking.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router(); // eslint-disable-line new-cap
 
 // Endpoint called by the client to check if a driver has selected his ride offer and wants to pick him up
-router.get('/:bookingId/status', function (req, res) {
+router.get('/:bookingId/', function (req, res) {
   let id = req.params.bookingId;
-
-  // Change
-  let answer = true;
-
-  res.statusCode = 200;
-  res.json(answer);
+  var db = require('../db');
+  var Offer = db.Mongoose.model('offer', db.OfferSchema, 'offer');
+  Offer.find({_id: id}).lean().exec(function(e,docs) {
+    res.statusCode = 200;
+    res.json({ acceptedByDriver: docs[0].acceptedByDriver});
+    res.end();
+  });
 });
 
 // Endpoint called by the client to make a ride offer for the drivers
@@ -51,15 +52,19 @@ router.post('/', function (req, res) {
 
 //Endpoint used by the client give a response to the driver he has been assigned
 router.post("/:bookingId", function(req, res) {
-  let name = req.body.name;
-  let lat = req.body.coordinates.lat;
-  let lon = req.body.coordinates.lon;
-
-  // Change
-  let answer = true;
-
-  res.statusCode = 200;
-  res.json(answer);
+  let id = req.params.bookingId;
+  let acceptedByClient = req.body.acceptedByClient;
+  var db = require('../db');
+  var Offer = db.Mongoose.model('offer', db.OfferSchema, 'offer');
+  Offer.findOneAndUpdate({_id: id}, {acceptedByClient: acceptedByClient}).lean().exec(function(e,docs){
+    if (err) {
+      res.status(500).json({ error: err.message });
+      res.end();
+      return;
+    }
+    res.statusCode = 200;
+    res.end();
+  });
 });
 
 
--- Engine/routes/rideOffer.js
+++ Engine/routes/rideOffer.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const router = express.Router(); // eslint-disable-line new-cap
 
-// Endpoint called by the drivers to choose a client to pick up for the ride
+// Endpoint called by the drivers to see a client to pick up for the ride
 router.get("/", function(req, res) {
-
   var db = require('../db');
   var Offer = db.Mongoose.model('offer', db.OfferSchema, 'offer');
   var offers = Offer.find({}).lean().exec(function(e,docs){
     res.statusCode = 200;
+    docs = docs.map(function (obj) {
+      return {id: obj._id, clientName: obj.clientName}
+    });
     res.json(docs);
     res.end();
- });
+  });
 });
 
-// Endpoint called by both the driver to get back status of ride (if the client accepted or not)
+// Endpoint called the driver to get back status of ride (if the client accepted or not)
 router.get("/:id/status", function(req, res) {
 
   // Change
@@ -23,11 +25,10 @@ router.get("/:id/status", function(req, res) {
   res.json(answer);
 });
 
-//Endpoint used by the client give a response to the driver he has been assigned
+//Endpoint used by the driver to give a response to the client
 router.post("/:rideOfferId", function(req, res) {
   let id = req.params.rideOfferId;
-  // Change
-  let answer = true;
+  let answer = req.body.answer;
 
   res.statusCode = 200;
   res.json(answer);
